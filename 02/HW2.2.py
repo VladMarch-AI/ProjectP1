fc8c47e88758179748f26f55b8256248dc6ff3f6
VladMarch-AI <postvlad1995@gmail.com>
2025-01-30 14:49:00 +0200

    HW2

--- 02/HW2.2.py
+++ 02/HW2.2.py
@@ -5,4 +5,6 @@ b = int(abcde // 1000 - a * 10)
 c = int(abcde // 100 - (a * 100 + b*10))
 d = int(abcde // 10 - (a*1000 + b*100 + c*10))
 e = int(abcde // 1 - (a*10000 + b*1000 + c*100 + d*10))
-print(e, d, c, b, a)
+
+abcde_mirror = (a + b*10 + c*100 + d*1000 + e*10000)
+print(abcde_mirror)
