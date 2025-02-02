4b4a906f4c9d1b92bd9cf5e8a69530bbc610a79d
VladMarch-AI <postvlad1995@gmail.com>
2025-02-02 11:33:46 +0200

    HW3

---
+++ 03/HW3.1.py
@@ -0,0 +1,20 @@
+a = input("Enter a first number: ")
+b = input("Enter a second number: ")
+
+if not a.isdigit() or not b.isdigit():
+    exit("No numbers entered")
+
+sym = input("Enter a simple math operation(+, -, *, /): ")
+
+if sym == "+":
+    print(int(a)+int(b))
+elif sym == "-":
+    print(int(a)-int(b))
+elif sym == "*":
+    print(int(a)*int(b))
+elif sym == "/" and int(b) != 0:
+    print(int(a)/int(b))
+elif int(b) == 0 and sym == "/":
+    exit("You can't divide by 0")
+else:
+    exit("Invalid symbol entered")
