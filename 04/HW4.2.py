10cc1cbe6e0e7e5cf26a9471b2fec7748c2b84ba
User_Vlad <postvlad1995@gmail.com>
2025-02-09 16:56:08 +0200

    HW4.2

--- 04/HW4.2.py
+++ 04/HW4.2.py
@@ -3,4 +3,4 @@ lst_1 = lst[::2]
 print("List: ", lst)
 if not lst:
     lst = [0]
-print("=>", sum(lst_1),"*", lst[-1], "=>", sum(lst_1) * lst[-1])
+print("=>", sum(lst_1), "*", lst[-1], "=>", sum(lst_1) * lst[-1])
