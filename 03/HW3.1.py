cded2622580e1a08a03187c902eabeea1a61aa2c
User_Vlad <postvlad1995@gmail.com>
2025-02-13 09:53:41 +0200

    HW3.1

--- 03/HW3.1.py
+++ 03/HW3.1.py
@@ -1,20 +1,21 @@
-a = input("Enter a first number: ")
-b = input("Enter a second number: ")
+first_n = input("Enter a first number: ")
+second_n = input("Enter a second number: ")
 
-if not a.isdigit() or not b.isdigit():
+if not first_n.isdigit() or not second_n.isdigit():
     exit("No numbers entered")
+first_n, second_n = int(first_n), int(second_n)
 
 sym = input("Enter a simple math operation(+, -, *, /): ")
 
 if sym == "+":
-    print(int(a)+int(b))
+    print(first_n + second_n)
 elif sym == "-":
-    print(int(a)-int(b))
+    print(first_n - second_n)
 elif sym == "*":
-    print(int(a)*int(b))
-elif sym == "/" and int(b) != 0:
-    print(int(a)/int(b))
-elif int(b) == 0 and sym == "/":
+    print(first_n * second_n)
+elif sym == "/" and second_n != 0:
+    print(first_n / second_n)
+elif second_n == 0 and sym == "/":
     exit("You can't divide by 0")
 else:
     exit("Invalid symbol entered")
