abd1080f9e99265dfb7193da19605135afcd9e34
User_Vlad <postvlad1995@gmail.com>
2025-02-14 13:41:01 +0200

    HW5.1

--- 05/HW5.1.py
+++ 05/HW5.1.py
@@ -1,16 +1,12 @@
 from keyword import kwlist
 from string import punctuation
 
-kwlist = list(kwlist)
-lst_w = kwlist[:]
+lst_w = list(kwlist)
 lst_w.append('get')
 
-lst_s = punctuation[:]
-lst_s = list(lst_s)
-lst_s.remove("_")
-lst_s.append(' ')
-print(lst_s)
+lst_s = punctuation
+lst_s = list(lst_s.replace('_', ' '))
 
 var = input("Enter new name for variable : ")
 var_1 = var + "nothing"
-print(not var[0].isdecimal() and var_1.islower() and not any(_ in list(var) for _ in lst_s ) and not var in lst_w and not "_" + "_" in var)
+print(not var[0].isdecimal() and var_1.islower() and not any(_ in list(var) for _ in lst_s) and var not in lst_w and not "_" + "_" in var)
