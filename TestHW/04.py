88fdbceef221a048fca1a23334d409aa4cfb82ad
User_Vlad <postvlad1995@gmail.com>
2025-02-17 17:35:48 +0200

    TestHW3

---
+++ TestHW/04.py
@@ -0,0 +1,7 @@
+x1 = float(input("Enter the coordinates "'x'" of the first point: "))
+y1 = float(input("Enter the coordinates "'y'" of the first point: "))
+x2 = float(input("Enter the coordinates "'x'" of the second point: "))
+y2 = float(input("Enter the coordinates "'y'" of the second point: "))
+
+distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+print("Distance: ", distance)
