a6141dda7623d29e69b2f77e36d87de127734177
User_Vlad <postvlad1995@gmail.com>
2025-02-17 15:07:41 +0200

    TestHW3

---
+++ TestHW/03.py
@@ -0,0 +1,8 @@
+from math import pi
+
+rad = float(input("Enter the radius of the circle:"))
+circuit = 2 * pi * rad
+area = pi * rad ** 2
+
+print("Circuit: ", circuit)
+print("Area of a circle: ", area)
