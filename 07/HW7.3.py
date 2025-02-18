6c404f68371b555a8db8834da7ed1d4a3d3529f0
User_Vlad <postvlad1995@gmail.com>
2025-02-18 16:59:18 +0200

    TestHW7.3

--- 07/HW7.3.py
+++ 07/HW7.3.py
@@ -3,6 +3,7 @@ def second_index(text, some_str):
   second_i = text.find(some_str, first_i + 1)
   return second_i if second_i != -1 else None
 
+
 assert second_index("sims", "s") == 3, 'Test1'
 assert second_index("find the river", "e") == 12, 'Test2'
 assert second_index("hi", "h") is None, 'Test3'
