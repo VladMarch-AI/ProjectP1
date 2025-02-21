2b043bfeff1800ee7b6e388ce07422bed1080e40
User_Vlad <postvlad1995@gmail.com>
2025-02-21 16:19:16 +0200

    HW8.2

--- 08/HW8.2.py
+++ 08/HW8.2.py
@@ -1,6 +1,7 @@
 def is_palindrome(text):
     from string import punctuation
-    my_text = "".join(item for item in text if item not in punctuation).lower().replace(" ","")
+    my_text = ("".join(item for item in text if item not in punctuation)
+               .lower().replace(" ", ""))
     return my_text == my_text[::-1]
 
 
