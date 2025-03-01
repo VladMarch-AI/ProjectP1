209921606e840cfa84c7276ace42441aed123b77
User_Vlad <postvlad1995@gmail.com>
2025-03-01 17:31:42 +0200

    HW10.1

--- 10/HW10.1.py
+++ 10/HW10.1.py
@@ -17,6 +17,6 @@ def some_gen(begin, end, func):
 
 
 gen = some_gen(2, 4, pow)
-assert isgenerator(gen) is True, 'Test1'
+assert isgenerator(gen) == True, 'Test1'
 assert list(gen) == [2, 4, 16, 256], 'Test2'
 print('OK')
